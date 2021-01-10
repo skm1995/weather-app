17839bfc27ac499a499a7e988283802a111f5def
Szymon Kozłowski <szymonkozlowski1995@gmail.com>
2021-01-10 14:03:37 +0100

    user interface

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,24 @@
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const location = search.value;
 
+    messageOne.textContent = 'Loading...';
+    messageTwo.textContent = '';
+
     fetch('http://localhost:3000/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
-                console.log(data.error);
+                messageOne.textContent = data.error;
+                messageTwo.textContent = '';
             } else {
-                console.log(data.location);
-                console.log(data.forecast);
+                messageOne.textContent = 'Location: ' + data.location;
+                messageTwo.textContent = 'Forecast: Temperature: ' + data.forecast.temperature + ', Feelslike: ' + data.forecast.feelslike;
             }
         });
     });
