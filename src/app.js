0f20e28f59a48d48eb6e4eabd1d62896ed23e667
Szymon Kozłowski <szymonkozlowski1995@gmail.com>
2021-01-10 12:56:57 +0100

    request params

--- src/app.js
+++ src/app.js
@@ -40,9 +40,28 @@ app.get('/help', (req, res) => {
 });
 
 app.get('/weather', (req, res) => {
+    if (! req.query.address) {
+        return res.send({
+            error: 'You must provide an address.'
+        });
+    }
+
     res.send({
         location: 'Philadelphia',
-        forecast: 'It is snowing.'
+        forecast: 'It is snowing.',
+        address: req.query.address,
+    });
+});
+
+app.get('/products', (req, res) => {
+    if (! req.query.search) {
+        return res.send({
+           error: 'You must provide a search term.'
+        });
+    }
+
+    res.send({
+       products: []
     });
 });
 
